29429479b5bf61372977e9552e0091fcd3d78b73
USAMI Kenta <tadsan@zonu.me>
2021-10-02 14:29:04 +0900

    Fix class name

--- src/Currency/EUR.php
+++ src/Currency/EUR.php
@@ -10,7 +10,7 @@ namespace Kane\Currency;
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
  * @link https://en.wikipedia.org/wiki/Euro
  */
-final class USD extends \Kane\Currency
+final class EUR extends \Kane\Currency
 {
     const SYMBOL = '€';
 }
